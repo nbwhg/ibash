62ac489448cb123b42536d626dc4b80cf0f61871
bigtree6688 <562139699@qq.com>
2016-12-04 12:47:05 +0800

    修复代码提醒的误操作

--- static/js/ueditor/ueditor.all.js
+++ static/js/ueditor/ueditor.all.js
@@ -13184,7 +13184,7 @@ UE.plugins['insertcode'] = function() {
                 rng = me.selection.getRange(),
                 pre = domUtils.findParentByTagName(rng.startContainer,'pre',true);
             if(pre){
-                pre.className = 'brush:'+lang+';toolbar:false;';
+                pre.className = 'prettyprint linenums lang-'+lang+';toolbar:false;';
             }else{
                 var code = '';
                 if(rng.collapsed){
@@ -13264,7 +13264,7 @@ UE.plugins['insertcode'] = function() {
 
                     });
                 }
-                me.execCommand('inserthtml','<pre id="coder"class="brush:'+lang+';toolbar:false">'+code+'</pre>',true);
+                me.execCommand('inserthtml','<pre id="coder"class="prettyprint linenums lang-'+lang+';toolbar:false">'+code+'</pre>',true);
 
                 pre = me.document.getElementById('coder');
                 domUtils.removeAttributes(pre,'id');
